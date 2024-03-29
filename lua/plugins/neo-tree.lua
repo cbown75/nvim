68c8e5ab23f3c1fda8aabb395671652a59d853a2
Christopher Bown <cbown75@gmail.com>
2024-03-29 08:26:04 -0700

    trying to fix keymaps

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,20 +4,20 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-    "s1n7ax/nvim-window-picker",
+		"MunifTanjim/nui.nvim",
+		"s1n7ax/nvim-window-picker",
 	},
 	config = function()
-        require("neo-tree").setup({
-            filesystem = {
-                filtered_items = {
-                    visible = true,
-                    hide_dotfiles = false,
-                    hide_gitignored = true,
-                },
-            },
-        })
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+				},
+			},
+		})
+		--		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		--		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
 }
