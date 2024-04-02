03bed21a937522b14683c7ed69dd84ab537696f3
Christopher Bown <cbown75@gmail.com>
2024-04-02 13:29:41 -0700

    chore(lua/plugins): configuring noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -11,13 +11,16 @@ return {
 					["cmp.entry.get_documentation"] = true,
 				},
 			},
-			-- you can enable a preset for easier configuration
 			presets = {
 				bottom_search = false, -- use a classic bottom cmdline for search
 				command_palette = false, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+			notify = {
+				enabled = true,
+				view = "notify",
 			},
 			routes = {
 				{
@@ -27,9 +30,9 @@ return {
 			},
 			cmdline = {
 				view = "cmdline_popup",
-				format = {
-					cmdline = { pattern = "^:", icon = "|>", lang = "vim", title = "" },
-				},
+				--format = {
+				--	cmdline = { pattern = "^:", icon = "|>", lang = "vim", title = "" },
+				--},
 			},
 			views = {
 				cmdline_popup = {
