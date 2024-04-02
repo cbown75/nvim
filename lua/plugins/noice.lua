d7e8a9adfd9a1ec10b5b0bc8df8005e82afbdc6d
Christopher Bown <cbown75@gmail.com>
2024-04-02 12:23:17 -0700

    chore(lua/plugins): trying to get the noice search in the floating window

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -55,7 +55,7 @@ return {
 		--opts.smart_move.enabled = true
 		opts.presets.lsp_doc_border = true
 		opts.presets.command_palette = true
-		opts.presets.bottom_search = true
+		opts.presets.bottom_search = false
 	end,
 
 	dependencies = {
